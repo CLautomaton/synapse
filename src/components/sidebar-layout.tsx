1811c2a75329ff67b04f6d42a3ecb5969b818768
George Dzavashvili <dzavashviligeorge@gmail.com>
2024-10-16 01:50:36 +0400

    fix: diasble sidebar scrolling

--- src/components/sidebar-layout.tsx
+++ src/components/sidebar-layout.tsx
@@ -9,12 +9,12 @@ const SideBarLayout: React.FC<LayoutProps> = ({ sidebar, main }) => {
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <aside className="w-64 bg-gray-800 text-white flex flex-col p-6">
+      <aside className="w-64 fixed h-full bg-gray-800 text-white flex flex-col p-6">
         {sidebar}
       </aside>
 
       {/* Main Content */}
-      <main className="flex-1 p-6 bg-gray-100">
+      <main className="ml-64 flex-1 p-6 bg-gray-100">
         {main}
       </main>
     </div>
