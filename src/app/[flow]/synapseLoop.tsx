af81fc17ac6c2ab697615f3677bd6ede6bf9ff65
George <dzavashviligeorge@gmail.com>
2024-12-09 01:01:21 +0400

    fix: add app id for clarity, remove the erroring out statement at the end

--- src/app/[flow]/synapseLoop.tsx
+++ src/app/[flow]/synapseLoop.tsx
@@ -58,14 +58,15 @@ const FullscreenComponent: React.FC<FullscreenComponentProps & { onImageClick: (
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <h1 className="text-3xl font-bold text-gray-800 mb-5">{title}</h1>
-      <div className="flex flex-col w-6/12 p-5 gap-10">
+      <div className="flex flex-col w-6/12 p-5 gap-10 justify-center align-middle">
         {apps.map((app, index) => (
           <button key={index} onClick={() => onImageClick(app.url)}>
             <img
-              className="border-4 border-transparent border-r-indigo-500 border-b-purple-500"
+              className="border-4 border-transparent border-r-indigo-500 border-b-purple-500 w-80 h-80"
               src={app.imageURL}
               alt={app.id}
             />
+            <p>{app.id}</p>
           </button>
         ))}
       </div>
@@ -109,7 +110,7 @@ export default function SynapseLoop({ selectedEntry, initialURL }: ClientProps) 
       const { type, score } = event.data;
       console.log("Type", type);
       console.log("Score", score);
-      console.log("Conditional", selectedEntry.flow[currentURLIndex.current + 1].conditional);
+      // console.log("Conditional", selectedEntry.flow[currentURLIndex.current + 1].conditional);
 
       if (currentURLIndex.current + 1 < selectedEntry.flow.length) {
         // Check if the condition is met for the next URL
