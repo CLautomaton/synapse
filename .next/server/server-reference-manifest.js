f629a33685f8624ab9a723dda0e4ef8e8265a3fd
George <dzavashviligeorge@gmail.com>
2026-01-07 15:28:57 +0400

    unstaged leftovers

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"17nzO6vwOaJzmjpG5u2oab5KyBiOs7PN14fctKxnW94=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GMtJoq8OT8IEt080ERG25i1RAitfDbsBrvtPlhGlL/s=\"\n}"
